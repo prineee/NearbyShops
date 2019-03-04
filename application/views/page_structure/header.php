71ba5ef7b1150738f2691ea90df07b988551240a
Achraf52 <achraf52@outlook.com>
2019-03-04 18:32:02 +0000

    Add minified versions of CSS and JS files.

--- application/views/page_structure/header.php
+++ application/views/page_structure/header.php
@@ -12,9 +12,8 @@
 
 		<!-- Bootstrap core CSS -->
 		<link href="<?php echo base_url('assets/bootstrap/css/bootstrap.min.css'); ?>" rel="stylesheet">
-
 		<!-- Custom CSS -->
-		<link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet">
+		<link href="<?php echo base_url('assets/css/style.min.css'); ?>" rel="stylesheet">
 
 	</head>
 
