5dabd101128724b9783dcd5f6e8a04a326f1d9bb
Achraf52 <achraf52@outlook.com>
2019-03-05 18:32:23 +0000

    Framework version update
    Apply the Bootstrap v4.3.1 security update to patch XSS vulnerability CVE-2019-8331
    Switch to using the default variation instead of the "bundle" as Popper.js is not used

--- application/views/page_structure/footer.php
+++ application/views/page_structure/footer.php
@@ -2,7 +2,7 @@
 	<!-- jQuery Core JavaScript -->
 	<script src="<?php echo base_url('assets/jquery/jquery.min.js'); ?>"></script>
 	<!-- Bootstrap Core JavaScript -->
-	<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.bundle.min.js'); ?>"></script>
+	<script src="<?php echo base_url('assets/bootstrap/js/bootstrap.min.js'); ?>"></script>
 	<!-- JS Cookie Plugin -->
 	<script src="<?php echo base_url('assets/js/JS-Cookie/v2.2.0/js.cookie.min.js'); ?>"></script>
 	<!-- Extra JavaScript Code -->
